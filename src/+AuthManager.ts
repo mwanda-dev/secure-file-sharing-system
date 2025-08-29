f0e9b0e2adefa1a53b6d25f631d4282f4f898ea4
Mwanda Chipongo <mwandachips00@gmail.com>
2025-08-29 23:17:10 +0200

    Defined Constants for salt and hash keys. Implemented function to generate a secure salt

--- src/+AuthManager.ts
+++ src/+AuthManager.ts
@@ -2,3 +2,19 @@ import { invoke } from "@tauri-apps/api/core";
 import { get, writable } from "svelte/store";
 
 const isAuthenticated = writable(false);
+
+const KEY_SALT = 'auth.salt';
+const KEY_HASH = 'auth.hash';
+
+async function get_store_value_key(key: string): Promise<Uint8Array | null> {
+    return invoke('plugin:store|get', { key }).then((val: any) => val ? new Uint8Array(Object.values(val)) : null);
+}
+
+async function set_store_value(key: string, value: Uint8Array): Promise<Void> {
+    await invoke('plugin:store|set', { key, value: Array.from(value) })
+}
+
+export async function generate_salt(): Promise<Uint8Array> {
+    const salt = crypto.getRandomValues(new Uint8Array(16));
+    return salt;
+}
