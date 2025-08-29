722f25e79ad3e6ace11eb4147bb06b310672d82c
Mwanda Chipongo <mwandachips00@gmail.com>
2025-08-29 23:18:29 +0200

    Minor correction from last commit

--- src/+AuthManager.ts
+++ src/+AuthManager.ts
@@ -10,7 +10,7 @@ async function get_store_value_key(key: string): Promise<Uint8Array | null> {
     return invoke('plugin:store|get', { key }).then((val: any) => val ? new Uint8Array(Object.values(val)) : null);
 }
 
-async function set_store_value(key: string, value: Uint8Array): Promise<Void> {
+async function set_store_value(key: string, value: Uint8Array): Promise<void> {
     await invoke('plugin:store|set', { key, value: Array.from(value) })
 }
 
@@ -18,3 +18,4 @@ export async function generate_salt(): Promise<Uint8Array> {
     const salt = crypto.getRandomValues(new Uint8Array(16));
     return salt;
 }
+
