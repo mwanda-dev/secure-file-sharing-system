13df32f27c2d9cdb5c8c87f51273958d4d6ab6d5
Mwanda Chipongo <mwandachips00@gmail.com>
2025-08-29 16:41:03 +0200

    Tauri App Skeleton

---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api/core";
+  import { onMount } from "svelte";
+
+  let message = "Hello Secure File Sharing";
+
+  onMount(() => {
+    // Will call tauri commands when DOM loads
+  });
+</script>
+
+<main>
+  <h1>{message}</h1>
+  <button on:click={() => alert("Test")}>Test Button</button>
+</main>
+
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  main {
+    text-align: center;
+  }
+</style>
