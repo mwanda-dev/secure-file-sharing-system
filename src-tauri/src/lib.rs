c50dbd4ee4e3c77eb64313e7040b428cae17d686
Mwanda Chipongo <mwandachips00@gmail.com>
2025-09-02 12:35:46 +0200

    Added file system plugin and changed security permissions from default ones for store

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -37,6 +37,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_store::Builder::default().build())
+        .plugin(tauri_plugin_fs::init())
         .invoke_handler(tauri::generate_handler![encrypt_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
